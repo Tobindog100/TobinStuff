a4df1d0a833f79c467e454ea9c3849857198b25f
Tobin <tobinconorprice@gmail.com>
2022-08-31 15:38:02 -0700

    Added Whip

---
+++ Items/SquirrelTail.cs
@@ -0,0 +1,43 @@
+using TobinStuff.Items.Projectiles;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TobinStuff.Items
+{
+	public class SquirrelTail : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			// This method quickly sets the whip's properties.
+			// Mouse over to see its parameters.
+			Item.DefaultToWhip(ModContent.ProjectileType<SquirrelTailPro>(), 20, 2, 4);
+
+			Item.shootSpeed = 4;
+			Item.rare = ItemRarityID.Green;
+
+			Item.channel = true;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				//.AddIngredient<ExampleItem>()
+				//.AddTile<Tiles.Furniture.ExampleWorkbench>()
+				.Register();
+		}
+
+		// Makes the whip receive melee prefixes
+		//public override bool MeleePrefix()
+		//{
+			//return true;
+		//}
+	}
+}
